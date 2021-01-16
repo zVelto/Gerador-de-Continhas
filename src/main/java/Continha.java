5ac55b5de067ef350928bf9f95dafcc260d832b5
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-16 16:25:22 -0300

    primeiro commit

---
+++ src/main/java/Continha.java
@@ -0,0 +1,65 @@
+public class Continha {
+
+    private int dividendo;
+    private int divisor;
+    private int resto;
+    private int algarismosDividendo;
+    private int algarismosDivisor;
+
+    public Continha(int algarismosDividendo, int algarismosDivisor, int resto) {
+        this.algarismosDividendo = algarismosDividendo;
+        this.algarismosDivisor = algarismosDivisor;
+        this.resto = resto;
+    }
+
+    public int getAlgarismosDividendo() {
+        return algarismosDividendo;
+    }
+
+    public void setAlgarismosDividendo(int algarismosDividendo) {
+        this.algarismosDividendo = algarismosDividendo;
+    }
+
+    public int getAlgarismosDivisor() {
+        return algarismosDivisor;
+    }
+
+    public void setAlgarismosDivisor(int algarismosDivisor) {
+        this.algarismosDivisor = algarismosDivisor;
+    }
+
+    public int getDivisor() {
+        return divisor;
+    }
+
+    public void setDivisor(int divisor) {
+        this.divisor = divisor;
+    }
+
+    public int getResto() {
+        return resto;
+    }
+
+    public void setResto(int resto) {
+        this.resto = resto;
+    }
+
+    public int getDividendo() {
+        return dividendo;
+    }
+
+    public void setDividendo(int dividendo) {
+        this.dividendo = dividendo;
+    }
+
+    @Override
+    public String toString() {
+        return "Continha{" +
+                "dividendo=" + dividendo +
+                ", divisor=" + divisor +
+                ", resto=" + resto +
+                ", algarismosDividendo=" + algarismosDividendo +
+                ", algarismosDivisor=" + algarismosDivisor +
+                '}';
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Continha continha = new Continha(5, 5, 0);
+        System.out.println(continha);
+    }
+}
